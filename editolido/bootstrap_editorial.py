1abda5336dbb15f2b8ef67b1e05360fcd317fb23
Eric Delord <edelord@gmail.com>
2018-02-09 18:22:55 +0100

    bumpversion

--- editolido/bootstrap_editorial.py
+++ editolido/bootstrap_editorial.py
@@ -30,7 +30,7 @@ class Logger(object):
         self.log(message, 1)
 
 logger = Logger()
-VERSION = '1.1.8'
+VERSION = '1.1.9'
 DOCUMENTS = os.path.join(os.path.expanduser('~'), 'Documents')
 AUTO_UPDATE_KEY = 'Mise à jour auto'
 

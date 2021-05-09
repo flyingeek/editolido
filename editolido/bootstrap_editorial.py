9cbc1e16c02d6c416d4ab14ad6770661f0eda34a
Eric Delord <68456+flyingeek@users.noreply.github.com>
2021-05-09 15:21:06 +0200

    bumpversion 1.4.8 -> 1.4.9

--- editolido/bootstrap_editorial.py
+++ editolido/bootstrap_editorial.py
@@ -33,7 +33,7 @@ class Logger(object):
 
 
 logger = Logger()
-VERSION = '1.4.8'
+VERSION = '1.4.9'
 DOCUMENTS = os.path.join(os.path.expanduser('~'), 'Documents')
 AUTO_UPDATE_KEY = 'Mise à jour auto'
 
